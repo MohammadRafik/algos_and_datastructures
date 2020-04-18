e630d02b93be5a61ea78660329d9826dd45690a0
unknown <rafik.mohammad1994@gmail.com>
2020-04-18 15:49:26 -0400

    solved the group anagrams problem finally, learned alot

--- leetcode_30day_challenge/week1/group_anagrams.py
+++ leetcode_30day_challenge/week1/group_anagrams.py
@@ -1,6 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        dict = {}
+        for str in strs:
+            sorted_str = ''.join(sorted(str))
+            if sorted_str in dict:
+                dict[sorted_str].append(str)
+            else:
+                dict[sorted_str] = [str]
 
-strs = ["tea","and","ace","ad","eat","dans"]
-s = strs[1]
-dict = {}
-print(type(sorted(s)))
-dict[sorted(s)].append(s)
+        return [dict[x] for x in dict]
+
+strs = ["tea","and","ace","ad","eat","dans", '','',""]
+print(Solution().groupAnagrams(strs))
+
